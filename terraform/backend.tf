baea9fcf374de065e62fa62e3bfe2ee14e20e169
manojsingh-manoj <manojsingh.manoj@gmail.com>
2025-06-04 20:46:29 +0200

    correct rg group name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-assignment1"
+    resource_group_name  = "rg-storage-assignment1"
     storage_account_name = "manojsingh04062025"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
