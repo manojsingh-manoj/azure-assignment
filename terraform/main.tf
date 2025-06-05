f9f3f31b9e87b51d4e804c06f823b817786872eb
manojsingh-manoj <manojsingh.manoj@gmail.com>
2025-06-05 19:26:27 +0200

    disable check

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,7 @@ resource "azurerm_public_ip" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 # Create a network security group
