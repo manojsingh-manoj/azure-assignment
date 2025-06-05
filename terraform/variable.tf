95abe384af24e872c6909f276e86246a4c877abf
manojsingh-manoj <manojsingh.manoj@gmail.com>
2025-06-05 06:38:42 +0200

    add vm code

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -11,7 +11,7 @@ variable "environment" {
 variable "vm_size" {
   description = "Azure VM size"
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_D2s_v3"
 }
 
 variable "open_ports" {
