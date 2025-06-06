58cd1c53bdf97cc19432687752da57aff5fc835e
manojsingh-manoj <manojsingh.manoj@gmail.com>
2025-06-06 05:33:26 +0200

    disable check

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_public_ip" "main" {
   name                = "pip-vm-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Basic"
 }
 
